4742931b3809e8428316e6e067e43396b47ba449
MartinInsight <martin.insightlogistics@gmail.com>
2025-06-25 09:25:40 -0700

    Update rail_map.js

--- js/rail_map.js
+++ js/rail_map.js
@@ -11,7 +11,7 @@ class RailCongestionMap {
         L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
             maxZoom: 18,
-            minZoom: 2
+            minZoom: 3
         }).addTo(this.map);
 
         this.map.setMaxBounds([
